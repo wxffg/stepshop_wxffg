457f9a7390dfad2ce1a089ea547406605271e8be
ITSTART <itstart@step.kz>
2025-04-29 18:24:28 +0500

    lesson_04: context

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from mainapp.views import index, contacts
 
+app_name = 'mainapp'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('contacts/', contacts, name='contacts'),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    title = 'Главная'
+
+    context = {
+        'title': title,
+    }
+    return render(request, 'index.html', context)
+
+def contacts(request):
+    return render(request, 'contacts.html')
