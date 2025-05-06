20e28501ea8e632bc15d2477ef3ab0d31642374f
ITSTART <itstart@step.kz>
2025-05-06 18:18:57 +0500

    lesson_05: models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True,
+        verbose_name='Имя',
+    )
+
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Катекория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name='Категория',
+    )
+
+    name = models.CharField(
+        max_length=128,
+        verbose_name='Имя продукта',
+    )
+
+    image = models.ImageField(
+        verbose_name='Изображение',
+        blank=True,
+        upload_to='product_images',
+    )
+
+    short_description = models.CharField(
+        max_length=128,
+        verbose_name='Краткое описание',
+        blank=True,
+    )
+
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True,
+    )
+
+    price = models.DecimalField(
+        verbose_name='Цена',
+        default=0,
+        max_digits=13,
+        decimal_places=2,
+    )
+
+    quantity = models.PositiveIntegerField(
+        verbose_name='Количество товара',
+        default=0,
+    )
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
