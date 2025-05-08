9905243367ad17542f4a6e0e629601494cb2469b
ITSTART <itstart@step.kz>
2025-05-08 18:42:54 +0500

    lesson_06: работа с данными

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 
+from mainapp.models import Product
+
+
 def index(request):
     title = 'Главная'
 
+    prods = Product.objects.all()[:3]
+
     context = {
         'title': title,
+        'products': prods,
     }
     return render(request, 'index.html', context)
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    title = 'Контакты'
+
+    context = {
+        'title': title,
+    }
+    return render(request, 'contacts.html', context)
--- stepshop/urls.py
+++ stepshop/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', include('mainapp.urls', namespace='mainapp')),
 
 ]
