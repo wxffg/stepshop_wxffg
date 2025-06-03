86de5ce7aeb96764f3e7127e1bd008e612d6855d
ITSTART <itstart@step.kz>
2025-06-03 18:42:37 +0500

    lesson_11: product, main_menu

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,14 +3,24 @@ from django.shortcuts import render
 from mainapp.models import Product, Category
 
 
+def get_main_menu(current='mainapp:index'):
+     return [
+        {'href': 'mainapp:index', 'name': 'Главная', 'active': 'mainapp:current'},
+        {'href': 'mainapp:products', 'name': 'Товары', 'active': 'mainapp:current'},
+        {'href': 'mainapp:about', 'name': 'О нас', 'active': 'mainapp:current'},
+        {'href': 'mainapp:contacts', 'name': 'Контакты', 'active': 'mainapp:current'},
+]
+
+
 def index(request):
     title = 'Главная'
 
-    prods = Product.objects.all()[:3]
+    prods = Product.objects.all()[:4]
 
     context = {
         'title': title,
         'products': prods,
+        'menu_links': get_main_menu(),
     }
     return render(request, 'index.html', context)
 
@@ -21,6 +31,8 @@ def about(request):
     context = {
         'title': title,
         'about': about,
+        'menu_links': get_main_menu('mainapp:about'),
+
     }
     return render(request, 'about.html', context)
 
@@ -30,6 +42,8 @@ def contacts(request):
 
     context = {
         'title': title,
+        'menu_links': get_main_menu('mainapp:contacts'),
+
     }
     return render(request, 'contacts.html', context)
 
@@ -42,20 +56,23 @@ def products(request):
     context = {
         'title': title,
         'products': prods,
-        'categories': categories
+        'categories': categories,
+        'menu_links': get_main_menu('mainapp:products'),
     }
     return render(request, 'products.html', context)
 
 
-def product(request):
+def product(request, pk):
     title = 'Товар'
 
-    prod = Product.objects.get(id=1)
-    same_prods = Product.objects.exclude(id=prod.id)
+    prod = Product.objects.get(id=pk)
+    same_prods = Product.objects.exclude(id=pk)
 
     context = {
         'title': title,
         'product': prod,
         'products': same_prods,
+        'menu_links': get_main_menu('mainapp:products'),
+
     }
     return render(request, 'product.html', context)
