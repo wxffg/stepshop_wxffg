592ffe7d29811bc261574a8d49bb02b9aa364c2f
ITSTART <itstart@step.kz>
2025-06-12 18:19:39 +0500

    lesson_14

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
-# Create your models here.
-class Basket:
-    pass
+class Basket(models.Model):
+    @property
+    def  product_cost(self):
+        return self.product_price * self.quantity
+
+    @property
+    def total_quantity(self):
+        _items = Basket.objects.filter(user=self.user)
+        return sum(list(map(lambda x: x.quantity, _items)))
+
+    @property
+    def total_cost(self):
+        _items = Basket.objects.filter(user=self.user)
+        return sum(list(map(lambda x: x.product_cost, _items)))
--- basketapp/views.py
+++ basketapp/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from basketapp.models import Basket
 from mainapp.models import Product
 
-
+@login_required
 def basket(request):
     if request.user.is_authenticated:
         basket_ = Basket.objects.filter(user=request.user)
@@ -14,7 +15,7 @@ def basket(request):
         return render(request, 'basketapp/basket.html, context')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-
+@login_required
 def basket_add(request, pk):
     product_ = get_object_or_404(Product, pk=pk)
 
@@ -28,6 +29,9 @@ def basket_add(request, pk):
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-
+@login_required
 def basket_remove(request, pk):
+    basket_record = get_object_or_404(Basket, pk=pk)
+    basket_record.delete()
+
     return render(request, 'basketapp/basket.html')
